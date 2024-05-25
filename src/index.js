86355ea3248e010fc37d310c8f899686f2a81caf
Adarsh Kumar <adarshkumar245@gmail.com>
2024-05-25 08:32:03 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -12,16 +12,19 @@ import "./utils/passport";
 const app = express();
 
 app.use(json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://moodspace.vercel.app"],
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
 connectToDB();
-
 app.use("/v1/user", routes.user);
 app.use("/v1/mood", routes.mood);
 app.use("/v1/journal", routes.journal);
-
 
 // app.get('*', function (req, res, next) {
 //   if (!req.originalUrl.startsWith("/api")) { // Adjust if your API uses a different prefix
@@ -36,14 +39,13 @@ app.use("/v1/journal", routes.journal);
 app.use((error, req, res, next) => {
   if (!error.statusCode) error.statusCode = 500;
 
-  if (error.statusCode === 301) { // Temporary workaround
+  if (error.statusCode === 301) {
+    // Temporary workaround
     return next(error); // Pass the error further along
-}
+  }
 
-  return res
-    .status(error.statusCode)
-    .json({ error: error.toString() });
-})
+  return res.status(error.statusCode).json({ error: error.toString() });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
